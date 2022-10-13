a1d6c5e039ea0d2a15bdf8a237263a6f94d1d9d8
Yasir Araphat <yasiraraphat@gmail.com>
2022-10-13 12:25:13 +0600

    2022-10-05

---
+++ 2022-10-05/index.php
@@ -0,0 +1,46 @@
+<pre>
+<?php 
+    // $data='I live in Dhaka';
+    // echo strlen($data);
+    // echo str_word_count($data);
+    // echo strrev($data);
+    // echo strpos($data,'in');
+    // echo str_replace('Dhaka','rangpur',$data);
+    // $data=10.58;
+    // $r=(int)$d;
+    // var_dump(is_numeric($data));
+    // echo max(15,24,85,8,45);
+    // echo pi();
+    // $d=50-100;
+    // echo abs(-6.5);
+    // echo sqrt(9);
+    // echo round(15.18);
+    // echo ceil(15.00);
+    // echo floor(15.6);
+    // echo rand(1,1000);
+    // define('c',15);
+    // echo c;
+    // echo (3**2);
+    // $d=4;
+    // $d+=2;
+    // echo $d;
+    // $d=1;
+    // echo $d++;
+    // echo "<br>";
+    // echo $d;
+    // 2>1 || 3>4 
+    // var_dump(2<>2);
+    // $d='';
+    // (2>8)? $d='ok':$d= 'error';
+    // echo $d;
+    // $d=17;
+    // if($d==15){
+    //     echo "Fifteen";
+    // }elseif($d==16){
+    //     echo "Sixteen";
+    // }elseif($d==17){
+    //     echo "Seventeen";
+    // }else{
+    //     echo "Error";
+    // }
+?>
